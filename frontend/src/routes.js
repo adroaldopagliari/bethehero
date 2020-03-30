44c8910af0d1e5191acaa868f206ba0535167c4e
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-03-30 03:33:15 -0300

    react front-end added

---
+++ frontend/src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Logon from './pages/Logon'
+import Register from './pages/Register'
+import Profile from './pages/Profile'
+import NewIcident from './pages/Newincident'
+
+export default function routes() {
+  return (
+    <BrowserRouter>
+      <Switch> //only one route
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIcident} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
